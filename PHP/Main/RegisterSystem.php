6875a94fb624a1679c9b20c081334333b64518cf
chiLee98 <56430392+chiLee98@users.noreply.github.com>
2019-12-09 12:55:59 +0000

    #36 Changes

--- PHP/Main/RegisterSystem.php
+++ PHP/Main/RegisterSystem.php
@@ -1,7 +1,7 @@
 <?php
 
-//require '/home/lc8884l/include/connection.php';
-require "../../PHP/connection.php";
+require '/home/lc8884l/include/connection.php';
+//require "../../PHP/connection.php";
 
 
 if (isset($_POST['regSubmit'])) {
